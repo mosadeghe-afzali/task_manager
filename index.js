9cdf0ad17ce67911288599862d782b0d710f0b3e
mosadeghe.afzali <mosadegheafzali@gmail.com>
2026-06-13 16:42:39 +0330

    refactore index.js

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+
+const express = require("express");
+const path = require("path");
+const router = require("./src/routes");
+
 const app = express();
-const path = require('path')
-const router = require('./src/routes');
-app.use(express.static(path.join(__dirname, 'src', 'public')));
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'src', 'views'))
-app.get('/', router);
-const port = process.env.APP_PORT
+const port = process.env.APP_PORT || 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src", "views"));
+
+app.use(express.static(path.join(__dirname, "src", "public"))); // دسترسی به پوشه استایل و عکس‌ها
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", router);
+
+app.use((req, res) => {
+  res.status(404).send("صفحه مورد نظر یافت نشد");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("خطایی در سمت سرور رخ داده است");
+});
 
 app.listen(port, () => {
-    console.log(`server is running on ${port}`)
-})
+  console.log(`سرور با موفقیت روی پورت ${port} روشن شد`);
+});
