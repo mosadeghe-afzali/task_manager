dcd3b8a7177896b5e5ffd3d08a0bf3a813700336
mosadeghe.afzali <mosadegheafzali@gmail.com>
2026-07-12 16:57:31 +0330

    api create project

--- src/controllers/ProjectController.js
+++ src/controllers/ProjectController.js
@@ -1,63 +1,51 @@
 const { validationResult } = require("express-validator");
-const projectService = require('../services/ProjectService');
-const teamService = require('../services/TeamService');
+const projectService = require("../services/ProjectService");
+const teamService = require("../services/TeamService");
 const { request } = require("express");
 
 const index = async (req, res) => {
-  const projects = await projectService.findMany({ limit: 2, selectFields: ['id', 'name'] });
-  console.log(projects)
-
-  res.render('projects/index.ejs', {
-    title: "xx"
-  })
-}
-
-
-const create = async (req, res) => {
-  const teams = await teamService.findMany();
-  const icons = [
-    { name: "folder", label: "عمومی" },
-    { name: "shopping-cart", label: "فروشگاه" },
-    { name: "users", label: "تیم" },
-    { name: "code", label: "توسعه" },
-    { name: "database", label: "دیتابیس" },
-  ];
-  res.render('projects/create.ejs', {
-    title: "ایجاد پروژه",
-    errors: [],
-    teams: teams,
-    request: req.body,
-    icons: icons
-
+  const projects = await projectService.findMany({
+    limit: 2,
+    selectFields: ["id", "name"],
   });
-}
+  console.log(projects);
+
+  res.render("projects/index.ejs", {
+    title: "xx",
+  });
+};
 
 const store = async (req, res, next) => {
   const errors = validationResult(req);
-  const teams = await teamService.findMany();
-  const icons = [
-    { name: "folder", label: "عمومی" },
-    { name: "shopping-cart", label: "فروشگاه" },
-    { name: "users", label: "تیم" },
-    { name: "code", label: "توسعه" },
-    { name: "database", label: "دیتابیس" },
-  ];
+
   if (!errors.isEmpty()) {
-    return res.render('projects/create.ejs', {
+    return res.render("projects/create.ejs", {
       title: "ایجاد پروژه",
       errors: errors.array(),
       request: req.body,
-      icons: icons
-    })
+    });
   }
 
-  const project = projectService.store(req.body)
-
-  res.redirect('/');
-}
+  try {
+    const project = projectService.store(req.body);
+    return res.status(201).json({
+      success: true,
+      message: "درخواست با موفقیت انجام شد.",
+      data: {
+        project,
+      },
+    });
+    
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 module.exports = {
   create,
   store,
-  index
-}
+  index,
+};
